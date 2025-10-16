c2f52b17d95f228c5eab7bae69ded900f078a1a2
NicolasVitek <nicolasvitek91@hotmail.com>
2025-10-16 15:49:22 -0300

    [NEW] Login auth

--- Java mobile application/app/src/main/java/com/example/javamobileapplication/LoginActivity.java
+++ Java mobile application/app/src/main/java/com/example/javamobileapplication/LoginActivity.java
@@ -4,24 +4,60 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
-import androidx.appcompat.widget.Toolbar;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import org.w3c.dom.Text;
+import androidx.appcompat.widget.Toolbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Objects;
+
 
 public class LoginActivity extends AppCompatActivity {
-    private TextView tv_register;
+    private EditText et_userEmail, et_userPassword;
+    private FirebaseAuth myAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        tv_register= findViewById(R.id.tv_register);
-        tv_register.setOnClickListener(registerListener);
+        et_userEmail=findViewById(R.id.et_userEmail);
+        et_userPassword=findViewById(R.id.et_userPassword);
+        Button btn_register = findViewById(R.id.btn_register);
+        btn_register.setOnClickListener(registerListener);
+        Button btn_login = findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(v->loginUser());
+        myAuth = FirebaseAuth.getInstance();
     }
     private final View.OnClickListener registerListener = v -> {
-        Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+        Intent intent = new Intent(this, RegisterActivity.class);
         startActivity(intent);
     };
+
+    private void loginUser() {
+        String email = et_userEmail.getText().toString().trim();
+        String password = et_userPassword.getText().toString().trim();
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Debe ingresar un email y una contraseña", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        myAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(this, "Inicio exitoso", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(this, LoginActivity.class));
+                        finish();
+                    } else {
+                        Toast.makeText(this, "Error al iniciar sesión: " +
+                                Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
 }
