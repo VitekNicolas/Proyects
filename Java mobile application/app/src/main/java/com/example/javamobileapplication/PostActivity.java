f4a18ca591c382fcf44fbc59e1694a4b4002e96d
NicolasVitek <nicolasvitek91@hotmail.com>
2025-10-18 21:09:28 -0300

    [NEW] PostActivity: location, type and date

--- Java mobile application/app/src/main/java/com/example/javamobileapplication/PostActivity.java
+++ Java mobile application/app/src/main/java/com/example/javamobileapplication/PostActivity.java
@@ -1,22 +1,25 @@
 package com.example.javamobileapplication;
 
 import android.Manifest;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 import java.io.File;
+import java.util.Calendar;
 
 public class PostActivity extends MenuActivity {
 
@@ -24,11 +27,16 @@ public class PostActivity extends MenuActivity {
     private static final int REQUEST_GALLERY = 100;
     private static final int REQUEST_IMAGE_CAPTURE = 101;
     private Uri imageUri;
+    private Spinner spinnerTipo;
+    private EditText etFecha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.post_activity);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+        etFecha = findViewById(R.id.et_fecha);
+        spinnerTipo = findViewById(R.id.spinner_tipo_reclamo);
         Button btnCamera = findViewById(R.id.btnCamera);
         Button btnSavePost=findViewById(R.id.btn_publish);
         Button btnGaleria = findViewById(R.id.btn_choose_image);
@@ -43,6 +51,7 @@ public class PostActivity extends MenuActivity {
         });
         btnGaleria.setOnClickListener(v -> abrirGaleria());
         btnSavePost.setOnClickListener(v -> guardarPublicacion());
+        etFecha.setOnClickListener(v -> mostrarSelectorFecha());
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
@@ -63,6 +72,19 @@ public class PostActivity extends MenuActivity {
         Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         startActivityForResult(intent, REQUEST_GALLERY);
     }
+
+    private void mostrarSelectorFecha() {
+        final Calendar calendario = Calendar.getInstance();
+        int año = calendario.get(Calendar.YEAR);
+        int mes = calendario.get(Calendar.MONTH);
+        int dia = calendario.get(Calendar.DAY_OF_MONTH);
+        DatePickerDialog dialog = new DatePickerDialog(this,
+                (view, year, month, dayOfMonth) -> {
+                    String fechaSeleccionada = dayOfMonth + "/" + (month + 1) + "/" + year;
+                    etFecha.setText(fechaSeleccionada);
+                }, año, mes, dia);
+        dialog.show();
+    }
     @Override
     public void onRequestPermissionsResult(int requestCode,
                                            @NonNull String[] permissions,
@@ -81,14 +103,11 @@ public class PostActivity extends MenuActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         ImageView imageView = findViewById(R.id.img_preview);
-        // 📸 Si la foto viene desde la cámara
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             imageView.setImageURI(imageUri);
         }
-
-        // 🖼️ Si la imagen viene desde la galería
         else if (requestCode == REQUEST_GALLERY && resultCode == RESULT_OK && data != null) {
-            imageUri = data.getData(); // obtenemos la URI de la imagen seleccionada
+            imageUri = data.getData();
             imageView.setImageURI(imageUri);
         }
     }
@@ -96,16 +115,16 @@ public class PostActivity extends MenuActivity {
     private void guardarPublicacion() {
         EditText et_description=findViewById(R.id.et_description);
         String descripcion = et_description.getText().toString().trim();
-        if (descripcion.isEmpty() || imageUri == null) {
-            Toast.makeText(this, "Debe agregar una descripción y una imagen", Toast.LENGTH_SHORT).show();
-            return;
+        String tipo = spinnerTipo.getSelectedItem().toString();
+        String fecha = etFecha.getText().toString();
+        if (descripcion.isEmpty() || imageUri == null || tipo.isEmpty() || fecha.isEmpty()) {
+            Toast.makeText(this, "Complete todos los campos", Toast.LENGTH_SHORT).show();
         }
         else{
             Toast.makeText(this, "Reclamos guardado correctamente", Toast.LENGTH_SHORT).show();
         }
         // Aquí guardarías en tu base de datos SQLite
         // Ejemplo: new Publicacion(descripcion, imageUri.toString(), ubicacion);
-        Toast.makeText(this, "Publicación guardada correctamente", Toast.LENGTH_SHORT).show();
         //finish();
     }
 }
