56cee6418681f75007506ef777c8fb7563ee4e09
NicolasVitek <nicolasvitek91@hotmail.com>
2025-10-21 23:09:10 -0300

    [NEW] App logo and settings menu

--- Java mobile application/app/src/main/java/com/example/javamobileapplication/MenuActivity.java
+++ Java mobile application/app/src/main/java/com/example/javamobileapplication/MenuActivity.java
@@ -4,6 +4,10 @@ import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -25,7 +29,23 @@ public class MenuActivity extends AppCompatActivity {
         } else if (id == R.id.action_historical) {
             startActivity(new Intent(this, PostListActivity.class));
             return true;
+        } else if (id==R.id.action_logout) {
+            cerrarSesion();
+            return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void cerrarSesion() {
+        FirebaseAuth.getInstance().signOut();
+        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(
+                this,
+                new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).build()
+        );
+        mGoogleSignInClient.signOut();
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
     }
 }
