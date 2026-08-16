cc8dfdcb7b5f3507a5648fab5d2eb0f5c1a48cbb
NicolasVitek <nicolasvitek91@hotmail.com>
2026-08-16 17:17:55 -0300

    [NEW] Backend: ignore bin/obj

--- E-commerce web application/Backend/Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
+++ E-commerce web application/Backend/Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58a41acee05e37b9ea6d9e35cf3c665e0d80e087")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39313e85bc28e9fc42bf8125727b65561a7692ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- E-commerce web application/Backend/Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
+++ E-commerce web application/Backend/Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58a41acee05e37b9ea6d9e35cf3c665e0d80e087")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39313e85bc28e9fc42bf8125727b65561a7692ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
