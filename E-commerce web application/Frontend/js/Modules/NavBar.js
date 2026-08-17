956983dd4db4c1d89ef46694e05d62687fbdb597
NicolasVitek <nicolasvitek91@hotmail.com>
2026-08-17 17:46:46 -0300

    [NEW] Frontend: delete old files

--- E-commerce web application/Frontend/js/Modules/NavBar.js
+++
@@ -1,9 +0,0 @@
-export const updateLiStyle=()=>{
-    const navItems = document.querySelectorAll('.nav-link');
-    navItems.forEach(item => {
-        item.addEventListener('click', function () {
-            navItems.forEach(nav => nav.classList.remove('active-nav-item'));
-            this.classList.add('active-nav-item');
-        });
-    });
-}
--- E-commerce web application/Frontend/js/components/ProductCart.js
+++
@@ -1,7 +0,0 @@
-export class ProductCart{
-    constructor(clientId, productId, amount){
-        this.clientId=clientId;
-        this.productId=productId;
-        this.amount=amount;
-    }
-}
